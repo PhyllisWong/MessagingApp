6e4d5114d50eaa6d1845e5482eb4f4484260dbbd
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-25 22:42:48 -0700

    Finished UI

---
+++ MessagingApp/ComposeViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ComposeViewController.swift
+//  MessagingApp
+//
+//  Created by djchai on 10/25/17.
+//  Copyright © 2017 Phyllis Wong. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func addPost(_ sender: Any) {
+        
+    }
+    
+    @IBAction func cancelPost(_ sender: Any) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
